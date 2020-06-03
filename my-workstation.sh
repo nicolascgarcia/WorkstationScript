a0c85317014e3d1996e680a396eaf29de4828c64
Nicolas Garcia <ni.car.garcia@gmail.com>
2020-06-03 16:39:18 -0300

    php 7.4 adicionado

--- my-workstation.sh
+++ my-workstation.sh
@@ -87,6 +87,8 @@ sudo mysql_secure_installation &&
 
 sudo apt update &&
 
+sudo apt install php php-cli php-fpm php-json php-pdo php-mysql php-zip php-gd  php-mbstring php-curl php-xml php-pear php-bcmath &&
+
 ## finalizando o processo ##
 
 sudo apt update && sudo apt dist-upgrade &&
